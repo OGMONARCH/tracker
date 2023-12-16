52f2d44a08b2e4eb08343abaca3349a6ad39b82f
OGMONARCH <149230226+OGMONARCH@users.noreply.github.com>
2023-12-16 14:00:25 +0100

    Initial commit

---
+++ src/pages/Desktop1.js
@@ -0,0 +1,286 @@
+import "./Desktop1.css";
+
+const Desktop1 = () => {
+  return (
+    <div className="desktop-1">
+      <div className="desktop-1-child" />
+      <div className="frame-parent">
+        <div className="search-parent">
+          <b className="search">Search...</b>
+          <img
+            className="basilsearch-outline-icon"
+            alt=""
+            src="/basilsearchoutline.svg"
+          />
+        </div>
+        <b className="cameron-rogers">Cameron Rogers</b>
+        <b className="manager">Manager</b>
+        <b className="b">{`>`}</b>
+        <img className="frame-child" alt="" src="/frame-4@2x.png" />
+        <img
+          className="carbonnotification-icon"
+          alt=""
+          src="/carbonnotification.svg"
+        />
+      </div>
+      <div className="frame-group">
+        <div className="search-for-order-id-status-et-parent">
+          <b className="search-for-order">Search for order ID, Status etc...</b>
+          <img
+            className="basilsearch-outline-icon1"
+            alt=""
+            src="/basilsearchoutline1.svg"
+          />
+        </div>
+        <div className="filters-parent">
+          <div className="filters">Filters</div>
+          <img className="ionfilter-icon" alt="" src="/ionfilter.svg" />
+        </div>
+        <div className="attachment-parent">
+          <div className="attachment">Attachment</div>
+          <img
+            className="majesticonsattachment"
+            alt=""
+            src="/majesticonsattachment.svg"
+          />
+        </div>
+        <div className="export-parent">
+          <div className="export">{`Export `}</div>
+          <div className="div">{`>`}</div>
+        </div>
+        <img className="miexport-icon" alt="" src="/miexport.svg" />
+      </div>
+      <div className="desktop-1-inner">
+        <div className="frame-container">
+          <img className="frame-item" alt="" src="/frame-7.svg" />
+          <b className="customer">Customer</b>
+          <b className="b1">1425</b>
+          <div className="parent">
+            <b className="b2">+21%</b>
+            <b className="increase">Increase</b>
+          </div>
+        </div>
+      </div>
+      <div className="group-div">
+        <div className="frame-container">
+          <img className="frame-item" alt="" src="/frame-71.svg" />
+          <b className="customer">Monthly Revenue</b>
+          <b className="b1">$2,167</b>
+          <div className="group">
+            <b className="b2">568</b>
+            <b className="increase">Orders</b>
+          </div>
+        </div>
+      </div>
+      <div className="desktop-1-inner1">
+        <div className="frame-container">
+          <img className="frame-item" alt="" src="/frame-72.svg" />
+          <b className="customer">Today Visitor</b>
+          <b className="b1">612</b>
+          <div className="container">
+            <b className="b2">+17</b>
+            <b className="increase">Today</b>
+          </div>
+        </div>
+      </div>
+      <div className="desktop-1-inner2">
+        <div className="frame-container">
+          <img className="frame-item" alt="" src="/frame-73.svg" />
+          <b className="customer">Today Sale</b>
+          <b className="b1">36</b>
+          <div className="parent1">
+            <b className="b2">-8%</b>
+            <b className="increase">Today</b>
+          </div>
+        </div>
+      </div>
+      <div className="desktop-1-inner3">
+        <div className="customer-statistics-parent">
+          <div className="customer-statistics">Customer Statistics</div>
+          <div className="ellipse-div" />
+          <div className="frame-child2" />
+          <div className="line-div" />
+          <div className="line-div" />
+          <div className="frame-child4" />
+          <div className="frame-child5" />
+          <div className="frame-child5" />
+          <div className="frame-child7" />
+          <div className="parent2">
+            <div className="div1">+70%</div>
+            <div className="div1">+70%</div>
+          </div>
+          <div className="wrapper">
+            <div className="div3">+30%</div>
+          </div>
+        </div>
+      </div>
+      <div className="desktop-1-inner4">
+        <div className="customer-statistics-parent">
+          <div className="available-trucks">Available Trucks</div>
+          <div className="ak-145746">AK-145746</div>
+          <div className="ak-740743">AK-740743</div>
+          <div className="ak-326783">AK-326783</div>
+          <div className="washinton-to-ilinois">Washinton to Ilinois</div>
+          <div className="abu-dhabi-to">Abu Dhabi to Dubai Hills Estate</div>
+          <div className="chelsea-to-manchester">Chelsea to manchester</div>
+          <div className="view-all">View all</div>
+          <div className="frame-child8" />
+          <div className="frame-child9" />
+          <div className="div4">65/100</div>
+          <div className="div5">30/100</div>
+          <div className="div6">82/100</div>
+        </div>
+      </div>
+      <b className="all-orders">All Orders</b>
+      <div className="completed">Completed</div>
+      <div className="in-progress">In Progress</div>
+      <div className="canceled">Canceled</div>
+      <div className="desktop-1-item" />
+      <div className="desktop-1-child1" />
+      <div className="order-id">Order ID</div>
+      <div className="customer1">Customer</div>
+      <div className="date">Date</div>
+      <div className="product">Product</div>
+      <div className="price">Price</div>
+      <div className="status">Status</div>
+      <div className="payment-meth">Payment meth.</div>
+      <div className="div7">{`>`}</div>
+      <div className="div8">{`>`}</div>
+      <div className="div9">{`>`}</div>
+      <div className="div10">{`>`}</div>
+      <div className="div11">{`>`}</div>
+      <div className="div12">{`>`}</div>
+      <div className="div13">{`>`}</div>
+      <div className="htes4325">HTES4325</div>
+      <div className="sdwe6321">SDWE6321</div>
+      <div className="ferw4793">FERW4793</div>
+      <div className="aqwe3252">AQWE3252</div>
+      <div className="jgtr8086">JGTR8086</div>
+      <div className="dwsn5127">DWSN5127</div>
+      <div className="klut7146">KLUT7146</div>
+      <div className="hmbc5095">HMBC5095</div>
+      <div className="rvcz2985">RVCZ2985</div>
+      <div className="dwck5647">DWCK5647</div>
+      <div className="klfd1642">KLFD1642</div>
+      <div className="zswe3672">ZSWE3672</div>
+      <div className="randy-michael">Randy Michael</div>
+      <div className="clement-mike">Clement Mike</div>
+      <div className="tory-victor">Tory Victor</div>
+      <div className="fowler-antony">Fowler Antony</div>
+      <div className="hector-bellerin">Hector Bellerin</div>
+      <div className="johnny-cage">Johnny Cage</div>
+      <div className="joyce-winters">Joyce Winters</div>
+      <div className="amber-cathy">Amber Cathy</div>
+      <div className="danny-neil">Danny Neil</div>
+      <div className="simpson-debby">Simpson Debby</div>
+      <div className="florina-dora">Florina Dora</div>
+      <div className="jose-moore">Jose Moore</div>
+      <div className="div14">07.12.2023</div>
+      <div className="div15">23.10.2023</div>
+      <div className="div16">21.06.2023</div>
+      <div className="div17">07.11.2023</div>
+      <div className="div18">17.12.2023</div>
+      <div className="div19">18.07.2023</div>
+      <div className="div20">19.06.2023</div>
+      <div className="div21">05.02.2023</div>
+      <div className="div22">11.06.2023</div>
+      <div className="div23">09.09.2023</div>
+      <div className="div24">19.04.2023</div>
+      <div className="div25">07.03.2023</div>
+      <div className="macbook-pro">Macbook pro</div>
+      <div className="puma-shirt">Puma Shirt</div>
+      <div className="nike-bag">Nike Bag</div>
+      <div className="air-freshner">Air freshner</div>
+      <div className="kg-dumbbell">50KG Dumbbell</div>
+      <div className="mary-kay">Mary kay</div>
+      <div className="hisense-tv">55’’ Hisense TV</div>
+      <div className="blender">Blender</div>
+      <div className="leather-shoe">Leather shoe</div>
+      <div className="gucci-bag">Gucci bag</div>
+      <div className="silver-necklace">Silver necklace</div>
+      <div className="zenik-wristwatch">Zenik wristwatch</div>
+      <div className="div26">$650.00</div>
+      <div className="div27">$230.00</div>
+      <div className="div28">$184.30</div>
+      <div className="div29">$21.80</div>
+      <div className="div30">$76.99</div>
+      <div className="div31">$26.99</div>
+      <div className="div32">$312.00</div>
+      <div className="div33">$56.00</div>
+      <div className="div34">$19.00</div>
+      <div className="div35">$531.45</div>
+      <div className="div36">$4312.00</div>
+      <div className="div37">$5321.00</div>
+      <div className="complete-wrapper">
+        <div className="complete">complete</div>
+      </div>
+      <div className="in-progress-wrapper">
+        <div className="in-progress1">In progress</div>
+      </div>
+      <div className="in-progress-container">
+        <div className="in-progress1">In progress</div>
+      </div>
+      <div className="in-progress-frame">
+        <div className="in-progress1">In progress</div>
+      </div>
+      <div className="in-progress-wrapper1">
+        <div className="in-progress1">In progress</div>
+      </div>
+      <div className="complete-container">
+        <div className="complete">complete</div>
+      </div>
+      <div className="canceled-wrapper">
+        <div className="complete">Canceled</div>
+      </div>
+      <div className="canceled-container">
+        <div className="complete">Canceled</div>
+      </div>
+      <div className="canceled-frame">
+        <div className="complete">Canceled</div>
+      </div>
+      <div className="complete-frame">
+        <div className="complete">complete</div>
+      </div>
+      <div className="complete-wrapper1">
+        <div className="complete">complete</div>
+      </div>
+      <div className="complete-wrapper2">
+        <div className="complete">complete</div>
+      </div>
+      <div className="credit-card">Credit card</div>
+      <div className="bank-transfer">Bank transfer</div>
+      <div className="credit-card1">Credit card</div>
+      <div className="paypal">Paypal</div>
+      <div className="credit-card2">Credit card</div>
+      <div className="bank-transfer1">Bank transfer</div>
+      <div className="paypal1">Paypal</div>
+      <div className="credit-card3">Credit card</div>
+      <div className="bank-transfer2">Bank transfer</div>
+      <div className="paypal2">Paypal</div>
+      <div className="credit-card4">Credit card</div>
+      <div className="bank-transfer3">Bank transfer</div>
+      <div className="group-parent">
+        <div className="frame-wrapper">
+          <div className="customer-statistics-parent">
+            <div className="top-countries">Top Countries</div>
+            <div className="frame-child10" />
+            <div className="frame-child11" />
+            <div className="frame-child12" />
+            <div className="frame-child13" />
+            <div className="australia">Australia</div>
+            <div className="united-state">United State</div>
+            <div className="asia">Asia</div>
+            <div className="africa">Africa</div>
+          </div>
+        </div>
+        <img
+          className="img-matplotlib-pie1-1-icon"
+          alt=""
+          src="/img-matplotlib-pie1-1@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Desktop1;
